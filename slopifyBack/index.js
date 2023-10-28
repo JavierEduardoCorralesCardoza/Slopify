2f0172669e47db8d046b467c20bf598a178cf18f
Jose David De la Garza Salas <jdgarza11@hotmail.com>
2023-10-27 18:35:19 -0600

    Creacion de carpetas de pantalla y updates de archivos Home

--- slopifyBack/index.js
+++ slopifyBack/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const signupRoute = require("./routes/signupRoute");
+const signinRoute = require("./routes/signinRoute.js")
 
-app.use("/signup", signupRoute);
+app.use("/home", signupRoute);
+app.use("/home", signinRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
